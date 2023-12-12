334b2c1a88553e0dc9a0967b3f68fa0b03bc3f7f
Javier Montes <javier.montes.carrera@gmail.com>
2023-12-12 23:56:06 +0800

    Adding tasks-list with JSON.stringify

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 // import Link from "next/link";
 import type { NextPage } from "next";
@@ -108,7 +108,7 @@ function MaintenanceExplorer() {
           </div>
         </div>
         <MaintenanceTask></MaintenanceTask>
-        <MaintenanceTask></MaintenanceTask>
+        {/* <MaintenanceTask></MaintenanceTask> */}
       </div>
     </>
   );
@@ -129,51 +129,80 @@ function AdminPanel() {
 }
 
 function MaintenanceTask() {
+  const [data, setData] = useState<{ result: object[] }>();
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/tasks-list")
+      .then(res => res.json())
+      .then(data => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading Task List...</p>;
+  if (!data) return <p>No tasks yet</p>;
+
+  const taskList = data.result;
+
+  if (typeof taskList != "undefined" && taskList?.length < 1) return <p>No tasks yet</p>;
+
   return (
     <div className="row mt-3">
       <h5 className="h5 borderTop">
         <div className="titleTask">Task Id #0 | Status</div>
       </h5>
-      <div className="col-md-2">
-        <div className="img-nft">
-          {/* <img src="https://ipfs.io/ipfs/bafybeifj3wz462zils26mztyepwfzhxlxe557k3sptm3yfcplorw7xlpoi" alt="img2" /> */}
-          <Image
-            alt="img2"
-            style={{ width: "100%" }}
-            width={0}
-            height={0}
-            src="https://ipfs.io/ipfs/bafybeifj3wz462zils26mztyepwfzhxlxe557k3sptm3yfcplorw7xlpoi"
-          />
-        </div>
-      </div>
-      <div className="col-md-10">
-        <ul className="p-2 tasklist">
-          <li>
-            <strong>Client Name:</strong> John
-          </li>
-          <li>
-            <strong>System Name:</strong> Airplane
-          </li>
-          <li>
-            <strong>System Cycles:</strong> 1000
-          </li>
-          <li>
-            <strong>Estimated Time:</strong> 3 days
-          </li>
-          <li>
-            <strong>Starting Time:</strong> 1 day
-          </li>
-          <li>
-            <strong>Cost (tokens):</strong> 1
-          </li>
-          <li>
-            <strong>Repairman (engineer):</strong> 0xa...
-          </li>
-          <li>
-            <strong>Quality Inspector (engineer):</strong> 0xa...
-          </li>
-        </ul>
-      </div>
+      {taskList?.map(task => {
+        return (
+          <div key={Math.random()}>
+            <div>{JSON.stringify(task)}</div>
+            <div className="col-md-2">
+              <div className="img-nft">
+                {/* <img src="https://ipfs.io/ipfs/bafybeifj3wz462zils26mztyepwfzhxlxe557k3sptm3yfcplorw7xlpoi" alt="img2" /> */}
+                <Image
+                  alt="img2"
+                  style={{ width: "100%" }}
+                  width={0}
+                  height={0}
+                  src="https://ipfs.io/ipfs/bafybeifj3wz462zils26mztyepwfzhxlxe557k3sptm3yfcplorw7xlpoi"
+                />
+              </div>
+            </div>
+            <div className="col-md-10">
+              <ul className="p-2 tasklist">
+                <li>
+                  <strong>Task Id:</strong> 0
+                </li>
+                <li>
+                  <strong>Client Name:</strong> John
+                </li>
+                <li>
+                  <strong>System Name:</strong> Airplane
+                </li>
+                <li>
+                  <strong>System Cycles:</strong> 1000
+                </li>
+                <li>
+                  <strong>Estimated Time:</strong> 3 days
+                </li>
+                <li>
+                  <strong>Starting Time:</strong> 1 day
+                </li>
+                <li>
+                  <strong>Cost (tokens):</strong> 1
+                </li>
+                <li>
+                  <strong>Repairman (engineer):</strong> 0xa...
+                </li>
+                <li>
+                  <strong>Quality Inspector (engineer):</strong> 0xa...
+                </li>
+              </ul>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
