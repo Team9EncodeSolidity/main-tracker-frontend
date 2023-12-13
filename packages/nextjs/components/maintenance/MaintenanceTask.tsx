42f9417cff01c3f80e056382f60be46af2f90421
Javier Montes <javier.montes.carrera@gmail.com>
2023-12-13 16:06:26 +0800

    Completing MaintenanceTask component to show task information.

--- packages/nextjs/components/maintenance/MaintenanceTask.tsx
+++ packages/nextjs/components/maintenance/MaintenanceTask.tsx
@@ -5,7 +5,22 @@ const URL = "http://localhost:8080";
 const PATH = "/tasks-list";
 
 export function MaintenanceTask() {
-  const [data, setData] = useState<{ result: object[] }>();
+  interface taskObject {
+    tokenId: string;
+    clientName: string;
+    systemName: string;
+    maintenanceName: string;
+    systemCycles: string;
+    estimatedTime: string;
+    startTime: any;
+    taskCost: string;
+    genStatus: string;
+    execStatus: string;
+    repairman: string;
+    qualityInspector: string;
+  }
+
+  const [data, setData] = useState<{ result: taskObject[] }>();
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -20,23 +35,24 @@ export function MaintenanceTask() {
   if (isLoading) return <p>Loading Task List...</p>;
   if (!data) return <p>No tasks yet</p>;
 
-  const taskList = data.result;
+  const taskList: taskObject[] = data.result;
 
   if (typeof taskList != "undefined" && taskList?.length < 1) return <p>No tasks yet</p>;
 
   return (
-    <div className="row mt-3">
-      <h5 className="h5 borderTop">
-        <div className="titleTask">Task Id #0 | Status</div>
-      </h5>
-      {taskList?.map((task: any) => {
+    <>
+      {taskList?.map((task: taskObject) => {
         return (
-          <div key={Math.random()}>
-            <small>
+          <div className="row mt-3" key={task?.tokenId}>
+            {/* {JSON.stringify(task)} */}
+            <h5 className="h5 borderTop">
+              <div className="titleTask">Task Id #0 | {task?.genStatus}</div>
+            </h5>
+            {/* <small>
               <div style={{ width: "300px" }}>
                 <code>{JSON.stringify(task).split(",").join("\n")}</code>
               </div>
-            </small>
+            </small> */}
             <b>ID: {task.tokenId}</b>
             <div className="col-md-2">
               <div className="img-nft">
@@ -52,37 +68,37 @@ export function MaintenanceTask() {
             <div className="col-md-10">
               <ul className="p-2 tasklist">
                 <li>
-                  <strong>Task Id:</strong> 0
+                  <strong>Task Id:</strong> {task?.tokenId}
                 </li>
                 <li>
-                  <strong>Client Name:</strong> John
+                  <strong>Client Name:</strong> {task?.clientName}
                 </li>
                 <li>
-                  <strong>System Name:</strong> Airplane
+                  <strong>System Name:</strong> {task?.systemName}
                 </li>
                 <li>
-                  <strong>System Cycles:</strong> 1000
+                  <strong>System Cycles:</strong> {task?.systemCycles}
                 </li>
                 <li>
-                  <strong>Estimated Time:</strong> 3 days
+                  <strong>Estimated Time:</strong> {task?.estimatedTime} days
                 </li>
                 <li>
-                  <strong>Starting Time:</strong> 1 day
+                  <strong>Starting Time:</strong> {new Date(task?.startTime * 1000).toUTCString()}
                 </li>
                 <li>
-                  <strong>Cost (tokens):</strong> 1
+                  <strong>Cost (tokens):</strong> {task?.taskCost}
                 </li>
                 <li>
-                  <strong>Repairman (engineer):</strong> 0xa...
+                  <strong>Repairman (engineer):</strong> {task?.repairman}
                 </li>
                 <li>
-                  <strong>Quality Inspector (engineer):</strong> 0xa...
+                  <strong>Quality Inspector (engineer):</strong> {task?.qualityInspector}
                 </li>
               </ul>
             </div>
           </div>
         );
       })}
-    </div>
+    </>
   );
 }
