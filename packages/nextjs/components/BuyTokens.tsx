690895df4abaca6338205363afc88ef193e3abca
Javier Montes <javier.montes.carrera@gmail.com>
2023-12-14 08:50:47 +0800

    Buy and approve using usePrepareContractWrite

--- packages/nextjs/components/BuyTokens.tsx
+++ packages/nextjs/components/BuyTokens.tsx
@@ -1,14 +1,105 @@
+import { useState } from "react";
+import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
+
 export function BuyTokens(params: any) {
-  console.log(JSON.stringify(params));
-  const handleButtonClick = () => {
-    // Handle button click logic here
+  // const [buy, setBuyEnabled] = useState(true);
+  const [approve, setAprEnabled] = useState(false);
+
+  const amount = "1000000000000000000";
+
+  const {
+    config: config_Buy,
+    error: prepareError_Buy,
+    isError: isPrepareError_Buy,
+  } = usePrepareContractWrite({
+    address: `${params.trackerContractAddress}`,
+    abi: params.trackerContractAbi,
+    functionName: "buyTokens",
+    args: [],
+    enabled: false,
+    value: 1n,
+  });
+
+  const {
+    config: config_Apr,
+    error: prepareError_Apr,
+    isError: isPrepareError_Apr,
+  } = usePrepareContractWrite({
+    address: `${params.tokenContractAddress}`,
+    abi: params.tokenContractAbi,
+    functionName: "approve",
+    args: [params.trackerContractAddress, amount],
+    enabled: false,
+  });
+
+  const {
+    data: data_Buy,
+    error: error_Buy,
+    isError: isError_Buy,
+    write: write_Buy,
+    reset: reset_Buy,
+  } = useContractWrite(config_Buy);
+  const { isLoading: isLoading_Buy, isSuccess: isSuccess_Buy } = useWaitForTransaction({
+    hash: data_Buy?.hash,
+  });
+
+  const {
+    data: data_Apr,
+    error: error_Apr,
+    isError: isError_Apr,
+    write: write_Apr,
+    reset: reset_Apr,
+  } = useContractWrite(config_Apr);
+  const { isLoading: isLoading_Apr, isSuccess: isSuccess_Apr } = useWaitForTransaction({
+    hash: data_Apr?.hash,
+  });
+
+  const approveSpend = async () => {
+    if (!approve) {
+      setAprEnabled(true);
+      console.log("Approving");
+      write_Apr?.();
+    }
+  };
+
+  if (isPrepareError_Buy) {
+    console.log(`PrepareError: ${prepareError_Buy?.message}`);
+  }
+  if (isError_Buy) {
+    console.log(`Error: ${error_Buy?.message}`);
+  }
+  if (isLoading_Buy) console.log("Buying token");
+  if (isSuccess_Buy) {
+    reset_Buy();
+    approveSpend();
+  }
+
+  if (isPrepareError_Apr) {
+    console.log(`PrepareError: ${prepareError_Apr?.message}`);
+  }
+  if (isError_Apr) {
+    console.log(`Error: ${error_Apr?.message}`);
+  }
+  if (isLoading_Apr) console.log("Approving token");
+  if (isSuccess_Apr) {
+    reset_Apr();
+    alert("Token successfully bought and approved!");
+  }
+
+  const handlePayTask = (e: any) => {
+    // some code here for paying the task
+    e.preventDefault();
+    console.log("Entro");
+    setAprEnabled(false);
+    write_Buy?.();
   };
 
   return (
     <div style={{ position: "fixed", bottom: 60, right: 100, zIndex: 1000 }}>
       {/* <div className="d-none">{params.trackerContractAddress}</div> */}
-      <button className="btn btn-primary" onClick={handleButtonClick}>
-        Buy Tokens
+      <button className="btn btn-primary" onClick={handlePayTask}>
+        {isLoading_Buy || isLoading_Apr ? "Buying..." : "Buy+Approve use of Tokens"}
+        {/* {isLoading ? "Buying..." : "Buy+Approve use of Tokens"} */}
       </button>
     </div>
   );
